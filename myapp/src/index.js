512f1ec2c48687429e8a053c46a36537948eb8b7
MharanurNitin <nitinmharanur@gmail.com>
2022-11-04 11:11:24 +0530

    normal bookapp

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-let Greeting=()=>{
-  return <h1>Hello world</h1>;
-}
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+const Booklist = () => {
+  return (
+    <section className="container">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+const Book = () => {
+  return (
+    <article className="book">
+      <Img className="img" />
+      <Title />
+      <Desc />
+    </article>
+  );
+};
+const Img = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
+      alt="book"
+    />
+  );
+};
+const Title = () => {
+  return <h3>I Love You to the Moon and Back</h3>;
+};
+const Desc = () => {
+  return <h5>William W Li MD</h5>;
+};
+ReactDom.render(<Booklist />, document.getElementById("root"));
