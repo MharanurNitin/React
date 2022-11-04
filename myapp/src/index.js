f83c8387112375018c6b82eb09c13ad3a9772556
MharanurNitin <nitinmharanur@gmail.com>
2022-11-04 13:20:30 +0530

    props

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,51 +1,43 @@
 import React from "react";
 import ReactDom from "react-dom";
+//CSS
 import "./index.css";
 
+//Setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/41n9-p6-PpL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "Reminders of Him: A Novel",
+  author: "Colleen Hoover",
+};
 const Booklist = () => {
   return (
     <section className="container">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props; //object destructuring.
+  // console.log(props);
   return (
     <article className="book">
-      <Img className="img" />
-      <Title />
-      <Desc />
+      <img src={img} alt="book" />,<h3>{title}</h3>,<h5>{author}</h5>
     </article>
   );
 };
-const src =
-  "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg";
-const Img = () => {
-  return <img src={src} alt="book" />;
-};
-const Title = () => {
-  return <h3>I Love You to the Moon and Back</h3>;
-};
 
-//jsx css styling(it has first priority than external css.that is it more powerful than external css.)
-const Desc = () => {
-  return (
-    <h5
-      style={{
-        color: "red",
-        fontWeight: "600",
-        letterSpacing: "2px",
-        fontFamily: "sans-serif",
-      }}
-      title="Thanks for visiting us."
-    >
-      William W Li MD
-    </h5>
-  );
-};
 ReactDom.render(<Booklist />, document.getElementById("root"));
